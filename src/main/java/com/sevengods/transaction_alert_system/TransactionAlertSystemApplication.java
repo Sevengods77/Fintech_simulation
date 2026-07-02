8fe723e31c3647722a9f153c82d2d4b5a457a0eb
Sevengods77 <vhg772004@gmail.com>
2026-07-02 20:21:20 +0530

    chore: initial commit from Spring Initializr

---
+++ src/main/java/com/sevengods/transaction_alert_system/TransactionAlertSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sevengods.transaction_alert_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionAlertSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionAlertSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sevengods/transaction_alert_system/TransactionAlertSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevengods.transaction_alert_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionAlertSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
